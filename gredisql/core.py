0d0f4d1c0e028305e06a3e8426da371e401310a2
sabbiramin113008 <sabbiramin.cse11ruet@gmail.com>
2023-08-24 11:15:48 +0600

    Initial commit

--- gredisql/core.py
+++ gredisql/core.py
@@ -6,3 +6,22 @@ date: 12 Mar 2023
 email: sabbir.amin@goava.com, sabbiramin.cse11ruet@gmail.com 
 
 """
+import json
+
+import redis
+from ariadne import load_schema_from_path, QueryType, make_executable_schema, graphql_sync, MutationType
+from flask import Flask, request, jsonify
+
+type_defs = load_schema_from_path("schema.graphql")
+query = QueryType()
+mutation = MutationType()
+
+REDIS_host = 'localhost'
+REDIS_port = 6379
+REDIS_db = 0
+REDIS_decode_response = True
+
+rr = redis.StrictRedis(host=REDIS_host,
+                       port=REDIS_port,
+                       db=REDIS_db,
+                       decode_responses=REDIS_decode_response)
