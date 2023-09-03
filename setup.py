d4bb0527f62d4314e73941bb590d19e7b610dd44
sabbiramin113008 <sabbiramin.cse11ruet@gmail.com>
2023-09-03 18:34:36 +0600

    Bumping version to 0.0.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='gredisql',
-    version='0.0.8',
+    version='0.0.9',
     author='Sabbir Amin',
     author_email='sabbiramin.cse11ruet@gmail.com',
     description='A GraphQL interface for Redis Database. ',
